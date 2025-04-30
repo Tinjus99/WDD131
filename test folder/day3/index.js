ec77dc2d24b6af77fa974ab0a08cfc00724c7648
Tinjus99 <156869630+Tinjus99@users.noreply.github.com>
2025-04-30 08:58:35 -0600

    day 3 class

---
+++ test folder/day3/index.js
@@ -0,0 +1,10 @@
+const Pi = 3.1415
+let Radius = 3
+function CircleArea(Radius){
+    return Pi * Radius * Radius
+}
+
+CircleArea(Radius)
+console.log(CircleArea(Radius)) 
+CircleArea(5)
+console.log(CircleArea(5))
